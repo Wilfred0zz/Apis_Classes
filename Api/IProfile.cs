4e584af00ce761a2698a438a8b3d523d952f1b6c
Wilfred0zz <Wilfredo.Recinos52@myhunter.cuny.edu>
2022-02-20 19:58:29 -0500

    Fixed issue with instance

---
+++ Api/IProfile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersApi
+{
+    public interface IProfile
+    {
+        string Name { get; set; }
+        int Age { get; set; }
+        char Gender { get; set; }
+        string Email { get; set; }
+
+        void PrintInfoEntered();
+
+        void CreateUser();
+
+        event EventHandler UserAdded;
+
+        int this[int profileId]
+        {
+            get;
+            set;
+        }
+
+    }
+
+}
